142c648bacf5c452c4cc0701f262896e9aa5bb5a
tutta-karlsson <133788604+tutta-karlsson@users.noreply.github.com>
2023-05-16 21:51:02 +0200

    Update PetClinicApplication.java, comment out @SpringBootApplication and Main content

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -25,11 +25,11 @@ package org.springframework.samples.petclinic;
  * @author Dave Syer
  *
  */
-@SpringBootApplication(proxyBeanMethods = false)
+//@SpringBootApplication(proxyBeanMethods = false)
 public class PetClinicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PetClinicApplication.class, args);
+		//SpringApplication.run(PetClinicApplication.class, args);
 	}
 
 }
